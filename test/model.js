cea4ee51d8c5b3e0383bbf51c7192e20340bd2fe
Ian Macalinao <me@ian.pw>
2014-07-16 12:15:13 -0500

    Add limiting number of returned documents (does not work right now)
    Signed-off-by: Ian Macalinao <me@ian.pw>

---
+++ test/model.js
@@ -0,0 +1,47 @@
+var async = require("async");
+var bay6 = require("../lib/");
+var expect = require("chai").expect;
+var mongoose = require("mongoose");
+var request = require("supertest");
+
+describe("Model", function() {
+  var app;
+  var model;
+
+  beforeEach(function() {
+    app = bay6();
+    app.options.prefix = "";
+    model = app.model("Document", {
+      title: String,
+      contents: String
+    });
+    mongoose.set("debug", true);
+  });
+
+  describe("#limit", function() {
+    it("should return a maximum of n documents", function(done) {
+      model.limit(5);
+
+      var server = app.serve(9000);
+
+      var Document = app.mongoose.model("Document");
+      async.each([1, 2, 3, 4, 5, 6], function(useless, done2) {
+        var doc = new Document({ title: "war and peace", contents: "yolo" });
+        doc.save(done2);
+      }, function(err) {
+        if (err) {
+          throw err;
+        }
+        request(server).get("/documents").end(function(err, res) {
+          expect(res.body.length).to.equal(5);
+          server.close();
+          done();
+        });
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    app.mongoose.db.dropDatabase(done);
+  });
+});
