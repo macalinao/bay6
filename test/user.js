8ca258b292e23266db0f5750bac7a5e33f6a3a57
Ian Macalinao <me@ian.pw>
2014-07-16 15:19:12 -0500

    Add user schema with password validation
    Signed-off-by: Ian Macalinao <me@ian.pw>

---
+++ test/user.js
@@ -0,0 +1,40 @@
+var expect = require("chai").expect;
+
+var bay6 = require("../lib/");
+
+describe("User", function() {
+  var app;
+  var User;
+
+  before(function() {
+    app = bay6();
+    app.serve(9000);
+    User = app.mongoose.model("User");
+  });
+
+  describe("#validPassword", function() {
+    var bob;
+    
+    before(function(done) {
+      bob = new User({username: "bob", password: "mfw"});
+      bob.save(function(err) {
+        if (err) {
+          throw err;
+        }
+        done();
+      });
+    });
+
+    it("should correctly validate the password", function() {
+      expect(bob.validPassword("mfw")).to.be.true;
+    });
+
+    it("should reject an invalid password", function() {
+      expect(bob.validPassword("MFW")).to.be.false;
+    });
+  });
+
+  afterEach(function() {
+    User.find().remove().exec();;
+  });
+});
